ca87300780405a6ce67aa84433933768f37db366
Jeff Kistler <jeff@jeffkistler.com>
2011-09-14 15:14:17 -0700

    Adding some markup indication and normalizing the project name.

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = 'bigrig',
+    name = 'BigRig',
     version = '0.1-pre',
     license = 'BSD',
     description = 'A pure Python ECMAScript 3 engine.',
-    long_description = read('README'),
+    long_description = read('README.rst'),
     author = 'Jeff Kistler',
     author_email = 'jeff@jeffkistler.com',
     packages = ['bigrig'],
@@ -20,6 +20,5 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP',
     ]
 )
